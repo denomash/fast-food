a014be117f557a3ae25c868ab1047e1f1d9f89bd
Dennis Macharia <dennismacharia4747@gmail.com>
2018-10-13 02:22:47 +0300

    [Feature #161195943] user can view ordered items

---
+++ static/js/history.js
@@ -0,0 +1,53 @@
+
+// Get stored token
+var stored_token = localStorage.getItem('x-access-token');
+
+var myHeaders = new Headers({
+	"Access-Control-Allow-Origin": "*/*",
+	"Content-Type": "application/json; charset=utf-8",
+	"x-access-token": stored_token
+});
+
+var myInit = { method: 'GET',
+               headers: myHeaders,
+               mode: 'cors',
+               cache: 'default' };
+
+
+var myRequest = new Request('http://localhost:5000/api/v2/users/orders');
+
+fetch(myRequest, myInit)
+.then((resp) =>	resp.json())
+.then((data) => {
+	console.log(data)
+	let orders = data.Orders
+	let output = `
+				<tr>
+				    <th>Image</th>
+				    <th>MealID</th>
+				    <th>Order Number</th>
+				    <th>Quantity</th>
+				    <th>Status</th>
+			    </tr> 
+				`
+	
+	orders.forEach((order) => {
+		output += `
+			<tr>
+		        <td><img src="${order.image}" height="60" width="100"></td>
+		        <td>${order.meal_id}</td>
+		        <td>${order.order_id}</td>
+		        <td>${order.quantity}</td>
+		        <td>${order.status}</td>
+		    </tr>
+		`;
+	});
+	document.getElementById('content').innerHTML = output;
+})
+
+function logout() {
+	// Remove data
+	localStorage.removeItem('x-access-token');
+
+	window.location = "./login.html";
+}
